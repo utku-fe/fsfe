4ad7ab714ee867af90866c3326350dbe4bdab8c4
utku-fe <utkutekin86@gmail.com>
2025-05-08 19:11:53 +0300

    add WebSocket server implementation and update dependencies

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,26 @@ server.on("request", app);
 server.listen(3000, function () {
   console.log("server started on port 3000");
 });
+
+/** begin websocket */
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ server: server });
+wss.on("connection", function connection(wss) {
+  const numClients = wss.clients.size;
+  console.log("clients connected ", numClients);
+
+  wss.boradcast(`current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("welcome to server");
+  }
+  ws.on("close", function close() {
+    console.log("a client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
